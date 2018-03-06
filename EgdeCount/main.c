46b7ec780436a1a42208cf90f06769ec905d2f9a
LordD98 <lorddelhey@gmail.com>
2018-03-06 22:05:40 +0100

    v1.1-PulseGen
    Added "Pulse-Generating-Mode" feature

---
+++ EgdeCount/main.c
@@ -0,0 +1,18 @@
+/*
+ * EgdeCount.c
+ *
+ * Created: 03.02.2018 20:27:48
+ * Author : s-del
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
